fee57c131987e7a98f0189b6ee47e566b5f47755
antoinemcgrath <antoine.mcgrath@gmail.com>
2015-10-12 15:04:29 -0700

    items page grey bar fix

--- public/js/item.js
+++ public/js/item.js
@@ -38,18 +38,18 @@ if (query){
 				return 0;
 			})
 		}
-		document.getElementById("itemHeader").innerHTML = '<div class="col-lg-12 col-md-12 col-sm-12" style="top: 2em; background-color: #D3D3D3; text-align: center; font-size: 1.8em; "><p style="padding: 10px; 30px; 0px; 30px;" class="col-lg-9 col-md-9 col-sm-9">' + res[0].title + '</p></div>';
+		document.getElementById("itemHeader").innerHTML = '<div class="col-lg-12 col-md-12 col-sm-12" style="background-color: #D3D3D3; text-align: center; font-size: 1.8em; margin-top:20px; padding-top:10px; padding-bottom:10px; "><p style="padding: 10px; 30px; 0px; 30px;" class="col-lg-9 col-md-9 col-sm-9">' + res[0].title + '</p></div>';
 
 		var elementString = "<a href='/links_reports/" + res[0].sha256 + "' download='"
 		+ res[0].ordercode
-		 +"_"+ res[0]._id.getFullYear()
-		  +"-"+ months[res[0]._id.getMonth()]
-		  +"-"+res[0]._id.getDate() 
-		  +".pdf"+
-		  "'><img src='/img/download.png' style='float: left; padding-right: 10px; width: 60px;' /><h4>Current Version: "
-		   + months[res[0]._id.getMonth()]
-		    + " " + res[0]._id.getDate()
-		     + ", " + res[0]._id.getFullYear() +"</h4></a>";
+		+"_"+ res[0]._id.getFullYear()
+		+"-"+ months[res[0]._id.getMonth()]
+		+"-"+res[0]._id.getDate() 
+		+".pdf"+
+		"'><img src='/img/download.png' style='float: left; padding-right: 10px; width: 60px;' /><h4>Current Version: "
+		+ months[res[0]._id.getMonth()]
+		+ " " + res[0]._id.getDate()
+		+ ", " + res[0]._id.getFullYear() +"</h4></a>";
 
 		if (res.length > 1) {
 			elementString += "<hr></br><h4> Additional Versions </h4><div style='margin-left: 20px;'>";
@@ -59,29 +59,29 @@ for(var i = 1; i < res.length; i++) {
 	elementString += "<a href='/links_reports/" + res[i].sha256 + "' download= '"
 	+ res[i].ordercode 
 	//+ "_" + res[i].parsed_metadata.title
-	 + "_" + res[i]._id.getFullYear()
-	 +"-"+months[res[i]._id.getMonth()]
-	 +"-"+res[i]._id.getDate()
-	 +".pdf"+
-	 "'><h5 style='padding:10px'>"
-	   + " " + months[res[i]._id.getMonth()]
-	    + " " + res[i]._id.getDate()
-	     + ", " + res[i]._id.getFullYear()	
-	     + "&nbsp;&nbsp;&nbsp;&nbsp;" 
-	      + res[i].title 
+	+ "_" + res[i]._id.getFullYear()
+	+"-"+months[res[i]._id.getMonth()]
+	+"-"+res[i]._id.getDate()
+	+".pdf"+
+	"'><h5 style='padding:10px'>"
+	+ " " + months[res[i]._id.getMonth()]
+	+ " " + res[i]._id.getDate()
+	+ ", " + res[i]._id.getFullYear()	
+	+ "&nbsp;&nbsp;&nbsp;&nbsp;" 
+	+ res[i].title 
 
-	      +"</h5></a>"
+	+"</h5></a>"
 }
 	//<a href='google.com' ><img src='/img/download.png'/ style='width: 3vw; float: left; padding-right: 10px;'></a>
 //elementString += "</div></br>"
-	
+
 };
 document.getElementById("outputItem").innerHTML = elementString;
 });
 };
 
 function runSearch() {
-   var query = document.getElementById("searchInput").value;	
-   window.location.href = "/result?q=" + query;
-   return false;
+	var query = document.getElementById("searchInput").value;	
+	window.location.href = "/result?q=" + query;
+	return false;
 };
