a3566a51e57b864b18dd1fa4e080352b5ceeba67
antoinemcgrath <antoine.mcgrath@gmail.com>
2016-04-16 00:46:57 -0700

    minor

--- app.js
+++ app.js
@@ -51,7 +51,7 @@ function uniq(a) {
 //app.use(bodyParser.urlencoded({ extended: false }))
 //app.use(bodyParser.json())
 app.set('views', __dirname + '/views');
-app.engine('html', require('ejs').renderFile);
+//app.engine('html', require('ejs').renderFile);
 app.use(express.static(__dirname + '/public'));
 
 app.set('view engine', 'html');
