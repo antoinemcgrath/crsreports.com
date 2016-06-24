fc12fb833c041a68b89746bf869dc9f3d1e6e66b
Antoine McGrath <antoine.mcgrath@gmail.com>
2016-06-23 22:46:51 -0700

    Update app.js

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var app = express();
+
 var config = require('./config.json')
 var fs = require('fs');
 var https = require('https');
@@ -9,38 +10,32 @@ var constants = require('constants');
 var bodyParser = require('body-parser');
 var MongoClient = require('mongodb').MongoClient;
 var ReadPreference = require('mongodb');
-<<<<<<< HEAD
-var path = require('path');
-
-// Gets unique items in a list
-=======
-
-var path = require('path');
-
 //var mongo = require('mongoskin');///
 //var db = mongo.db("mongodb://localhost:27017/crs", {native_parser:true});///
 //var url = 'mongodb://localhost:27017/crs';///
+
 //var db = null;
+
 //console.log(config.mongo);
 
->>>>>>> f9d9e455f351557249d0cf3102558f6c1635844d
+var path = require('path');
+
 function uniq(a) {
-    var seen = {};
-    return a.filter(function(item) {
-        return seen.hasOwnProperty(item) ? false : (seen[item] = true);
-    });
+        var seen = {};
+        return a.filter(function(item) {
+                return seen.hasOwnProperty(item) ? false : (seen[item] = true);
+        });
 }
 
-// Middleware that adds a number of security enhancements
 app.use(helmet());
+//app.use(bodyParser.urlencoded({ extended: false }))
 
-<<<<<<< HEAD
-=======
+
 //app.use(bodyParser.json())
->>>>>>> f9d9e455f351557249d0cf3102558f6c1635844d
 app.set('views', __dirname + '/views');
 app.engine('html', require('ejs').renderFile);
 app.use(express.static(__dirname + '/public'));
+
 app.set('view engine', 'html');
 
 app.get('/', function(req, res) {
@@ -59,6 +54,8 @@ app.get('/download', function(req,res){
     var hash = req.query.hash;
     db.collection('reports').findOne({sha256: hash, parsed_metadata.serve : '1'}, function(err, result){
         if(err || !result) {
+             //XXX: Error
+
             console.log("error");
             res.redirect(301, '/');
             return;
@@ -67,84 +64,101 @@ app.get('/download', function(req,res){
         var date = result.parsed_metadata.date;
         var months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sept', 'Oct', 'Nov', 'Dec']
         if (oc && date) {
-            var filename = oc + "_" + date.getUTCDate() + "-" + months[date.getUTCMonth()] + "-" + date.getUTCFullYear() + ".pdf";
-            var stream = fs.createReadStream('public/links_reports/' + path.normalize(hash));
-            stream.on('error',function(err) {
-                console.log("error: a report in the database WAS NOT FOUND ON DISK");
-                res.redirect(302, '/');
-            });
+            var filename = oc + "_" + date.getUTCDate() + "-" + months[date.getUTCMonth()] + "-" + date.getUTCFullYear() + ".\
+pdf";
+
+                var stream = fs.createReadStream('public/links_reports/' + path.normalize(hash));
+                stream.on('error',function(err) {
+                   console.log("error: a report in the database WAS NOT FOUND ON DISK");
+                   res.redirect(302, '/');
+                });
             res.setHeader('Content-disposition', 'inline; filename="' + filename + '"');
-            res.setHeader('Content-type', 'application/pdf');
-            stream.pipe(res);
+                res.setHeader('Content-type', 'application/pdf');
+          //  res.end('public/links_reports/' + path.normalize(hash), filename);
+                stream.pipe(res);
+          //  res.download('public/links_reports/' + path.normalize(hash), filename);
         } else {
-            // XXX: Error
+          //  XXX: Error
         }
     });
-});
 
-app.get('/search', function(req, res){
-    var query = req.query.q;
-    db.collection("reports").aggregate([
+
+});
+app.get('/search', function(req, res){  var query = req.query.q;
+//      var regex = new RegExp(query, 'i')
+//      db.bind('reports');
+        db.collection("reports").aggregate([
         { $match: { $text : { $search: query}} },
+//      { $limit: 100 },
         { $sort: {"parsed_metadata.date": -1 }},
         { $group: {'_id': '$parsed_metadata.ordercode',
                    title : {$first : "$parsed_metadata.title"},
                    sha256 : {$first : "$sha256"},
                    date : {$first : "$parsed_metadata.date"},
                    score: {$first : {$meta: "textScore"}}}},
+//         first score, date, then title
         { $sort: {"score": -1, "date": -1, "title": 1, "_id": 1}},
-    ], function(err, results){
-        if(err){
-            console.log(err);
-            return res.status(500).send("There is an error");
-        }
-        var unique = {};
-        var distinct = [];
-	
-        res.send(results);
-    });
-})
+//      { $limit: 10 },
+        ], function(err, results){
+                if(err){
+                        console.log(err);
+                        return res.status(500).send("There is an error");
+                }
+                var unique = {};
+                var distinct = [];
 
-// Item page (this is requested by JavaScript on result.html)
-app.get('/getitem', function(req, res){
-    var query = req.query.q;
-    
-    db.collection("reports").aggregate(
-        [{$match: {"parsed_metadata.ordercode":req.query.q}},
-         {$group: {'_id': "$parsed_metadata.date",
-                   title : {$first : "$parsed_metadata.title"},
-                   sha256 : {$first : "$sha256"},
-                   ordercode : {$first : "$parsed_metadata.ordercode"}}} ],
-        function(err, results) {
-            if(err){
-		console.log(err);
-		return res.status(500).send("There is an error");
-            }
-            res.send(results);
+                res.send(results);
         });
+
 })
 
-// Report info page (this is requested by JavaScript on result.html)
+
+
+//logic for item page
+app.get('/getitem', function(req, res){
+        var query = req.query.q;
+        //db.bind('reports');
+
+        db.collection("reports").aggregate(
+           [{$match: {"parsed_metadata.ordercode":req.query.q}},
+            {$group: {'_id': "$parsed_metadata.date",
+                      title : {$first : "$parsed_metadata.title"},
+                      sha256 : {$first : "$sha256"},
+                      ordercode : {$first : "$parsed_metadata.ordercode"}}} ],
+        function(err, results) {
+           if(err){
+              console.log(err);
+              return res.status(500).send("There is an error");
+           }
+           res.send(results);
+        });
+
+})
+
+///break off onto a nesssw resource get file renders layouts page formating from additional file
+
+
+
+
+// Code fos report info page
 app.get('/report', function(req, res) {
+//      db.bind('reports');
         db.collection("reports").find({'parsed_metadata.ordercode': req.query.id}).toArray(function(err, items) {
-            var title = items[0]['parsed_metadata']['title'];
-            var rptid = items[0]['parsed_metadata']['ordercode'];
-            var dates = [];
-            var sourcevar = [];
-            var urlvar = [];
+//      title
+        var title = items[0]['parsed_metadata']['title'];
+        var rptid = items[0]['parsed_metadata']['ordercode'];
+        var dates = [];
+        var sourcevar = [];
+        var urlvar = [];
 
 
-            items.forEach(function(it){
-		sourcevar.push(it['source']);
-	    });
-            var sourcevarTxt = "";
-            uniq(sourcevar).forEach(function(d){
+        items.forEach(function(it){
+//          sourcevar += it['parsed_metadata']['source'] + "<br/>"
+sourcevar.push(it['source']);
+});
+        var sourcevarTxt = "";
+        uniq(sourcevar).forEach(function(d){
                 sourcevarTxt += d + "<br/>"
-<<<<<<< HEAD
-            });
-
-            items.forEach(function(it){
-=======
         });
 
 
@@ -152,28 +166,23 @@ app.get('/report', function(req, res) {
 
 
         items.forEach(function(it){
->>>>>>> f9d9e455f351557249d0cf3102558f6c1635844d
                 urlvar.push(it['url']);
-            });
-            var urlvarTxt = "";
-            uniq(urlvar).forEach(function(d){
+        });
+        var urlvarTxt = "";
+        uniq(urlvar).forEach(function(d){
                 urlvarTxt += d + "<br/>"
-            });
+        });
 
-            items.forEach(function(it){
-		dates.push(it['parsed_metadata']['date']);
-	    });
-            var dateTxt = "";
-            uniq(dates).forEach(function(d){
+
+
+
+        items.forEach(function(it){
+//          dates += it['parsed_metadata']['date'] + "<br/>"
+dates.push(it['parsed_metadata']['date']);
+});
+        var dateTxt = "";
+        uniq(dates).forEach(function(d){
                 dateTxt += d + "<br/>"
-<<<<<<< HEAD
-            });
-	    
-            res.send("<h1>" + items[0]['parsed_metadata']['title'] + "</h1>" +
-                     "<h2>Order Code</h2>" + rptid +
-                     "<h2>Dates</h2>" + dateTxt);
-	});
-=======
         });
 
 
@@ -187,7 +196,6 @@ app.get('/report', function(req, res) {
 
 //      res.send(items);
 });
->>>>>>> f9d9e455f351557249d0cf3102558f6c1635844d
 });
 
 var port;
