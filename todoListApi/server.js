01f0a7d517e672e1b488a41376443714ac848675
antoinemcgrath <antoine.mcgrath@gmail.com>
2018-04-04 16:47:21 -0400

    Updated API port, & applied acceptable nginx syntax of crsreports-api

--- todoListApi/server.js
+++ todoListApi/server.js
@@ -1,7 +1,7 @@
 
 var express = require('express'),
     app = express(),
-    port = process.env.PORT || 3000,
+    port = process.env.PORT || 8085,
     mongoose = require('mongoose'),
     Task = require('./api/models/todoListModel'), //created model loading here
     bodyParser = require('body-parser');
