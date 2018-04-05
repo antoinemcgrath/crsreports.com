d3536ed0ead2df239bea3c6ecfcff3b59748ef5b
antoinemcgrath <antoine.mcgrath@gmail.com>
2018-04-04 18:06:18 -0700

    Improved error note

--- todoListApi/server.js
+++ todoListApi/server.js
@@ -27,5 +27,5 @@ console.log('todo list RESTful API server started on: ' + port);
 
 
 app.use(function(req, res) {
-    res.status(404).send({url: req.originalUrl + ' not found \n line  /n break for help tweet me @ https://twitter.com/AGreenDCBike'})
+    res.status(404).send({url: req.originalUrl + ' not found. CRSReports.com API error. For help tweet me @ https://twitter.com/AGreenDCBike'})
 });
